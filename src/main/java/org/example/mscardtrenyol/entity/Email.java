35c7a834e2a6e2eb68185b4b5be5ea6d9ac76501
zarifaalili <alilizarifa@gmail.com>
2025-08-27 13:29:25 +0400

    email ile bagli claslar elave edildi

---
+++ src/main/java/org/example/mscardtrenyol/entity/Email.java
@@ -0,0 +1,26 @@
+package org.example.mscardtrenyol.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.mscardtrenyol.enums.Status;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "emails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private LocalDateTime createdAt;
+    private Status status;
+
+
+}
---
+++ src/main/java/org/example/mscardtrenyol/repository/EmailRepository.java
@@ -0,0 +1,10 @@
+package org.example.mscardtrenyol.repository;
+
+import org.example.mscardtrenyol.entity.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailRepository extends JpaRepository<Email, Long> {
+    Optional<Email> findByEmail(String email);
+}
